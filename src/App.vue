a2563d5dd4b15b20acc193a216194d0c8c0a59f3
guoxiaoting <guoxiaoting949@163.com>
2018-07-01 12:58:12 +0800

    add code

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div style="display: flex; justify-content: center">
+      <Marquee :tipList="tipList" :showAvatar="false" :width="500" ></Marquee>
+    </div>
+  </div>
+</template>
+
+<script>
+import Marquee from './components/Marquee'
+export default {
+  name: 'App',
+  components: {
+    Marquee
+  },
+  data: function () {
+    return {
+      tipList: [
+        '张三获得了一台笔记本电脑',
+        '二狗子获得了一台笔记本电脑',
+        'Tracy获得了一箱冰淇淋'
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Marquee.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="marquee" :style="{width: width+'px'}">
+    <ul class="marquee_ul" :style="{width: marqueeWidth+'px'}">
+      <li :key="index" v-for="(tip,index) in tipList" class="marquee-li">
+        <img v-if="showAvatar" :src="avatar" width="43" height="40"/>
+        <span :style="{ paddingRight: distance}">{{tip}}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'marquee',
+  data: function () {
+    return {
+      marqueeWidth: ''
+    }
+  },
+  props: {
+    tipList: {
+      type: Array,
+      default: function () {
+        return []
+      }
+    },
+    avatar: {
+      type: String,
+      default: '../../static/head.png'
+    },
+    width: {
+      type: Number,
+      default: 700
+    },
+    distance: {
+      type: String,
+      default: '100px'
+    },
+    showAvatar: {
+      type: Boolean,
+      default: true
+    }
+  },
+  mounted () {
+    const tips = document.getElementsByClassName('marquee-li')
+    let totalWidth = 0
+    for (let i = 0; i < tips.length; i++) {
+      if (this.showAvatar) {
+        totalWidth += tips[i].children[0].width + tips[i].children[1].offsetWidth
+        tips[i].style.width = tips[i].children[0].width + tips[i].children[1].offsetWidth + 'px'
+      } else {
+        totalWidth += tips[i].children[0].offsetWidth
+        tips[i].style.width = tips[i].children[0].offsetWidth + 'px'
+      }
+    }
+    this.marqueeWidth = totalWidth
+    const rule = `@keyframes mymove{
+                0%   {left:${this.width}px;}
+                100% {left:-${this.marqueeWidth - this.width}px;}
+              }`
+    if (document.styleSheets && document.styleSheets.length) {
+      document.styleSheets[0].insertRule(rule, 0)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .marquee{
+    line-height: 40px;
+    margin: 10px 0 80px;
+    overflow: hidden;
+    transition: all 0.5s;
+  }
+  .marquee_ul{
+    height: 50px;
+    position: relative;
+    animation:mymove 20s linear infinite;
+    -moz-animation:mymove 20s linear infinite; /* Firefox */
+    -webkit-animation:mymove 20s linear infinite; /* Safari and Chrome */
+    -o-animation:mymove 20s linear infinite; /* Opera*/
+  }
+  .marquee_ul li{
+    height: 50px;
+    text-align: center;
+    display: flex;
+    float: left;
+  }
+  .marquee_ul li span{
+    display: block;
+  }
+</style>
